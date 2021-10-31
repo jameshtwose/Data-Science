76b75a0597348300089f342d0e55d68331976ea5
James <james@Jamess-MacBook-Air.local>
2021-10-31 14:00:15 +0100

    Adapting the simple nba dash app example to test different scatter plot types in plotly

--- Dashboarding/python/dash_simple_nba_simple_app.py
+++ Dashboarding/python/dash_simple_nba_simple_app.py
@@ -3,6 +3,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
 
 all_teams_df = pd.read_csv('src_data/shot_dist_compiled_data_2019_20.csv')
 
@@ -13,15 +16,42 @@ team_names.sort()
 app.layout = html.Div([
     html.Div([dcc.Dropdown(id='group-select', options=[{'label': i, 'value': i} for i in team_names],
                            value='TOR', style={'width': '140px'})]),
-    dcc.Graph('shot-dist-graph', config={'displayModeBar': False})])
+    dcc.Graph('shot-dist-graph', config={'displayModeBar': False}),
+    dcc.Graph('shot-dist-multi-graph', config={'displayModeBar': False}),
+    ])
 
 @app.callback(
     Output('shot-dist-graph', 'figure'),
     [Input('group-select', 'value')]
 )
 def update_graph(grpname):
-    import plotly.express as px
     return px.scatter(all_teams_df[all_teams_df.group == grpname], x='min_mid', y='player', size='shots_freq', color='pl_pps')
+
+@app.callback(
+    Output('shot-dist-multi-graph', 'figure'),
+    [Input('group-select', 'value')]
+)
+def update_multi_graph(grpname):
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Add traces
+    fig.add_trace(
+        go.Scatter(x=all_teams_df.loc[all_teams_df.group == grpname, "min_mid"],
+                   y=all_teams_df.loc[all_teams_df.group == grpname, "player"],
+                   line=dict(color="#743de0"),
+                   name="min_mid"),
+                   secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(x=all_teams_df.loc[all_teams_df.group == grpname, "min_start"],
+                   y=all_teams_df.loc[all_teams_df.group == grpname, "player"],
+                   line=dict(color="#CC5500"),
+                   name="min_start"),
+                   secondary_y=True,
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=False)
