e601f677857c1e69dabceec0337072bc2b6f1087
James Twose <jamestwose@Mac-mini.home>
2022-10-24 12:08:33 +0200

    adding some investigations into speech recognition options

---
+++ Machine_learning/NLP/speech-to-text.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#%%
+import speech_recognition as sr
+from dotenv import find_dotenv, load_dotenv
+# obtain path to "english.wav" in the same folder as this script
+from os import path, environ
+
+load_dotenv(find_dotenv())
+
+#%%
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audio_files/Note_20221022_1630.wav")
+# AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
+# AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
+
+# %%
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+
+#%%
+use_pocket_sphinx=True
+if use_pocket_sphinx:
+    # recognize speech using Sphinx
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+# %%
+use_GSR=False
+if use_GSR:
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# %%
+use_WIT=False
+if use_WIT:
+    # recognize speech using Wit.ai
+    # WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
+    WIT_AI_KEY = environ["WIT_API_KEY"]
+    try:
+        print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+    except sr.UnknownValueError:
+        print("Wit.ai could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Wit.ai service; {0}".format(e))
+# %%
+use_IBM=False
+if use_IBM:
+    # recognize speech using IBM Speech to Text
+    IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+    IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
+    try:
+        print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+    except sr.UnknownValueError:
+        print("IBM Speech to Text could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from IBM Speech to Text service; {0}".format(e))
