6d13c7c1852bbace0c771a20b94b84c53319ad52
James Twose <jamestwose@Mac-mini.home>
2023-05-16 22:51:46 +0200

    trying out some gradio demos - pretty neat/ handy to show off simple demos of NLP

---
+++ Machine_learning/NLP/gradio_demos/text_generation_gradio.py
@@ -0,0 +1,23 @@
+import gradio as gr
+from transformers import pipeline
+
+generator = pipeline('text-generation', model='gpt2')
+
+def generate(text):
+    result = generator(text, max_length=100, num_return_sequences=10)
+    return result[0]["generated_text"]
+
+examples = [
+    ["The Moon's orbit around Earth has"],
+    ["The smooth Borealis basin in the Northern Hemisphere covers 40%"],
+    ["The way you speak is"]
+]
+
+demo = gr.Interface(
+    fn=generate,
+    inputs=gr.inputs.Textbox(lines=5, label="Input Text"),
+    outputs=gr.outputs.Textbox(label="Generated Text"),
+    examples=examples
+)
+
+demo.launch(debug=True)
